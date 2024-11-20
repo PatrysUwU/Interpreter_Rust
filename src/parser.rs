49593ac0bcdbf7f1aca2331c428b5df996221e34
tzgfryziu <patryk.fryzlewicz@gmail.com>
2024-11-20 18:01:02 +0100

    One character tokens

---
+++ src/parser.rs
@@ -0,0 +1,119 @@
+use std::fmt;
+use std::fmt::{write, Formatter};
+
+mod lexer;
+use lexer::Lexer;
+
+#[derive(Debug)]
+#[allow(dead_code, non_camel_case_types)]
+pub enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // one or two character tokens.
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+
+    GREATER,
+    GREATE_REQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    // literals.
+    IDENTIFIER(String),
+    STRING(String),
+    NUMBER(f64),
+
+    // keywords.
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    EOF,
+}
+
+enum LiteralValue {
+    Int(i32),
+    String(String),
+    Null,
+}
+
+impl fmt::Display for LiteralValue {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match &self {
+            LiteralValue::Int(x) => { write!(f, "{x}") }
+            LiteralValue::String(x) => { write!(f, "{x}") }
+            LiteralValue::Null => { write!(f, "NULL") }
+        }
+    }
+}
+pub struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: LiteralValue,
+    line: i32,
+}
+
+impl Token {
+    fn new(token_type: TokenType, lexeme: String, literal: LiteralValue, line: i32) -> Self {
+        Token {
+            token_type,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?} {} {}", self.token_type, self.lexeme, self.literal)
+    }
+}
+
+
+pub struct Parser {
+    lexer: Lexer,
+    tokens: Vec<Token>,
+}
+
+impl Parser {
+    pub fn new(source: String) -> Self {
+        Parser {
+            lexer: Lexer::new(source),
+            tokens: Vec::new(),
+        }
+    }
+
+    pub fn print_tokens(&mut self) {
+        self.lexer.tokenize();
+        for token in self.lexer.tokens.iter() {
+            println!("{}", token);
+        }
+    }
+}
